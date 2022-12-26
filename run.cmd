a829aa924c2dc8d2336ab5af13b57eb540f0fbac
jtsato <jorge.takeshi.sato@gmail.com>
2022-12-25 21:15:40 -0300

    refactor(mutation): add java mutation presentation

--- run.cmd
+++ run.cmd
@@ -1,4 +1,4 @@
 @echo off
 docker-compose down
-start http://localhost:9000/output/mutation-testing-netcore.html?%RANDOM%
+start http://localhost:9000/output/mutation-testing-java.html?%RANDOM%
 docker-compose up -d
