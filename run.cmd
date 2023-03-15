e2529f533a4a0d151f991bec36b3c93874fe0432
jtsato <jorge.takeshi.sato@gmail.com>
2023-03-15 00:39:00 -0300

    feat(wip): work in progress

--- run.cmd
+++ run.cmd
@@ -1,5 +1,5 @@
 @echo off
 docker-compose down
-start http://localhost:9000/output/01-agile.html?%RANDOM%
+start http://localhost:9000/output/03-solid.html?%RANDOM%
 docker-compose build --no-cache
 docker-compose up -d
