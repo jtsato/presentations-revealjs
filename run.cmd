167d7518abd7f151deceef6d57a31b5e04991ef5
Jorge Takeshi Sato <jorge.takeshi.sato@gmail.com>
2024-12-08 00:44:38 -0300

    fix(pipeline): fix docker compose command

--- run.cmd
+++ run.cmd
@@ -1,5 +1,5 @@
 @echo off
-docker-compose down
+docker compose down
 start http://localhost:9000/output/coverage-and-mutation-testing.html?%RANDOM%
-docker-compose build --no-cache
-docker-compose up -d
+docker compose build --no-cache
+docker compose up -d
